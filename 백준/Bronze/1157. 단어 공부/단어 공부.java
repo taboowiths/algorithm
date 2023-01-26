27ac0d65cf3fe2839bc44d43c74bd5b45756d46c
taboowiths <85155789+taboowiths@users.noreply.github.com>
2023-01-26 23:46:58 +0900

    [Bronze I] Title: 단어 공부, Time: 280 ms, Memory: 21040 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -6,39 +6,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] alpha = new int[26];
         String s = br.readLine();
-        String test = s.toLowerCase();
+        String test = s.toUpperCase();
 
         for (int i = 0; i < test.length(); i++) {
             char ch = test.charAt(i);
-            alpha[ch-97]++;
+            alpha[ch-'A']++;
         }
 
-        int result = checkValue(alpha);
-        if (result == -1) {
-            System.out.println('?');
-        } else {
-            String ans = String.valueOf((char)(result+97));
-            System.out.println(ans.toUpperCase());
-        }
-        br.close();
-    }
-
-    public static int checkValue (int[] alpha) {
-        int maxIdx = -1;
-        int max = Integer.MIN_VALUE;
-        // 최대값 확인
+        int max = -1;
+        char ans = '?';
         for (int i = 0; i < 26; i++) {
             if (alpha[i] > max) {
-                maxIdx = i;
                 max = alpha[i];
+                ans = (char) (i+65);
+            } else if (alpha[i] == max) {
+                ans = '?';
             }
         }
-        // 중복 확인
-        for (int i = 0; i < 26; i++) {
-            if (i != maxIdx && alpha[i] == max) {
-                return -1;
-            }
-        }
-        return maxIdx;
+        System.out.println(ans);
+        br.close();
     }
 }
