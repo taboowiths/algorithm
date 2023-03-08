4cc6081223a3e93652a52c1938df8f4a3188c0ab
taboowiths <85155789+taboowiths@users.noreply.github.com>
2023-03-08 14:00:05 +0900

    [level 2] Title: 피로도, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,26 @@
+class Solution {
+    static int N, ans;
+    static boolean[] visited;
+    public int solution(int k, int[][] dungeons) {
+        N = dungeons.length;
+        visited = new boolean[N];
+        dfs(0, k, dungeons);
+        int answer = ans;
+        return answer;
+    }
+    public static void dfs (int cnt, int k, int[][] dungeons) {
+        if (cnt > ans) {
+            ans = cnt;
+        }
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            if (dungeons[i][0] <= k) {
+                k-=dungeons[i][1];
+                dfs(cnt+1, k, dungeons);
+                k+=dungeons[i][1];
+            }
+            visited[i] = false;
+        }
+    }
+}
