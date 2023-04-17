3a2e3dbcf4623df48253dcedb9cf595e067fb2cf
taboowiths <85155789+taboowiths@users.noreply.github.com>
2023-04-17 17:44:40 +0900

    [unrated] Title: 추억 점수, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i=0; i<name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+        int[] answer = new int[photo.length];
+        for (int i=0; i<photo.length; i++) {
+            int sum = 0;
+            for (int j=0; j<photo[i].length; j++) {
+                if (map.get(photo[i][j]) != null) {
+                    sum += map.get(photo[i][j]);
+                }
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
+}
