4f74d8a09d0b22b77afef2f31ce0794f1a6ede87
taboowiths <85155789+taboowiths@users.noreply.github.com>
2023-05-28 23:50:04 +0900

    [Gold III] Title: 파티, Time: 680 ms, Memory: 18940 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,14 +1,14 @@
 import java.io.*;
 import java.util.*;
-public class Main {
+public class Main { 
     static List<int[]>[] graph;
-    static int N, X;
+    static int N;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        X = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
         graph = new List[N+1];
         for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
         for (int i = 0; i < M; i++) {
@@ -22,18 +22,15 @@ public class Main {
         int[] distance = new int[N+1];
 
         int max = Integer.MIN_VALUE;
-        int max_idx = -1;
         for (int i = 1; i <= N; i++) {
-//            dijkstra(i, distance);
-            int result = dijkstra(i, distance);
+            int result = 0;
+            result += (dijkstra(i, X, distance) + dijkstra(X, i, distance));
             max = Math.max(max, result);
         }
         System.out.println(max);
     }
 
-    public static int dijkstra (int start, int[] distance) {
-        int result = 0;
-
+    public static int dijkstra (int start, int end, int[] distance) {
         for (int j = 1; j <= N; j++) distance[j] = Integer.MAX_VALUE;
         boolean[] visited = new boolean[N+1];
 
@@ -49,7 +46,7 @@ public class Main {
             }
 
             visited[cur] = true;
-            if (cur == X) break;
+            if (cur == end) break;
 
             for (int[] j : graph[cur]) {
                 if (!visited[j[0]] && distance[j[0]] > distance[cur] + j[1]) {
@@ -57,35 +54,6 @@ public class Main {
                 }
             }
         }
-//        System.out.println(start +  " start -> X " + Arrays.toString(distance));
-//        System.out.println(distance[X]);
-        result += distance[X];
-
-        for (int j = 1; j <= N; j++) distance[j] = Integer.MAX_VALUE;
-        visited = new boolean[N+1];
-
-        distance[X] = 0;
-        for (int i = 1; i <= N; i++) {
-            int min = Integer.MAX_VALUE;
-            int cur = X;
-            for (int j = 1; j <= N; j++) {
-                if (!visited[j] && min > distance[j]) {
-                    min = distance[j];
-                    cur = j;
-                }
-            }
-
-            visited[cur] = true;
-            if (cur == start) break;
-
-            for (int[] j : graph[cur]) {
-                if (!visited[j[0]] && distance[j[0]] > distance[cur] + j[1]) {
-                    distance[j[0]] = distance[cur] + j[1];
-                }
-            }
-        }
-//        System.out.println(start +  " X -> start " + Arrays.toString(distance));
-//        System.out.println(distance[start]);
-        return result += distance[start];
+        return distance[end];
     }
 }
