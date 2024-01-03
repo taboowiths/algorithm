f59af6d53fe932f9ff3fb52e4f4f872b215ea2dd
taboowiths <85155789+taboowiths@users.noreply.github.com>
2024-01-03 14:34:25 +0900

    [level 3] Title: 풍선 터트리기, Time: 14.16 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] balloons) {
+        int min = Integer.MAX_VALUE;
+        int minIdx = -1;
+        for (int i = 0; i < balloons.length; i++) {
+            if (balloons[i] < min) {
+                min = balloons[i];
+                minIdx = i;
+            }
+        }
+        
+        int cnt = 1;
+        int subMin = balloons[0];
+        for (int i = 1; i < minIdx; i++) {
+            if (balloons[i] < subMin) {
+                subMin = balloons[i];
+                cnt++;
+            }
+        }
+        
+        cnt++;
+        subMin = balloons[balloons.length - 1];
+        for (int i = balloons.length - 2; i >= minIdx; i--) {
+            if (balloons[i] < subMin) {
+                subMin = balloons[i];
+                cnt++;
+            }
+        }
+        
+        // System.out.println(cnt);
+        
+        
+        return cnt;
+    }
+}
