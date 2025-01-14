98ca84e1ef3d6625be19214ff7924ca1f325fc9f
Kang JeongHyun <85155789+taboowiths@users.noreply.github.com>
2025-01-14 11:02:51 +0900

    [Gold IV] Title: 최단경로, Time: 2176 ms, Memory: 114692 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,43 +1,37 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Node {
-        int vertex, cost;
-        Node link;
-        public Node (int vertex, int cost, Node link) {
-            this.vertex = vertex;
-            this.cost = cost;
-            this.link = link;
-        }
-    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine());
-        Node[] g=new Node[V+1];
+        int start = Integer.parseInt(br.readLine());
+
+        List<int[]>[] graph = new List[V+1];
+        for (int i = 0; i <= V; i++) graph[i] = new ArrayList<>();
+
         int[] distance = new int[V+1];
+        for (int i = 1; i <= V; i++) distance[i] = Integer.MAX_VALUE;
+
         boolean[] visited = new boolean[V+1];
-        for (int i = 1; i <= V; i++) {
-            distance[i] = Integer.MAX_VALUE;
-        }
-        for (int i = 0; i < E; i++) {
+
+        for (int i = 1; i <= E; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            g[from] = new Node(to, cost, g[from]);
+            int weight = Integer.parseInt(st.nextToken());
+            graph[from].add(new int[] {to, weight});
         }
-        distance[K] = 0;
+
+        distance[start] = 0;
         for (int i = 1; i <= V; i++) {
-            int min = Integer.MAX_VALUE;
-            int cur = 0;
+            int min = Integer.MAX_VALUE; int cur = 0;
             for (int j = 1; j <= V; j++) {
                 if (!visited[j] && min > distance[j]) {
                     min = distance[j];
@@ -47,15 +41,19 @@ public class Main {
 
             visited[cur] = true;
 
-            for (Node j = g[cur]; j!=null; j=j.link) {
-                if (!visited[j.vertex] && distance[j.vertex] > distance[cur] + j.cost) {
-                                          distance[j.vertex] = distance[cur] + j.cost;
+            for (int[] j : graph[cur]) {
+                if (!visited[j[0]] && distance[j[0]] > distance[cur] + j[1]) {
+                    distance[j[0]] = distance[cur] + j[1];
                 }
             }
+
         }
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= V; i++) {
-            if (distance[i] == Integer.MAX_VALUE) System.out.println("INF");
-            else System.out.println(distance[i]);
+            if (distance[i] == Integer.MAX_VALUE) sb.append("INF");
+            else sb.append(distance[i]);
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
