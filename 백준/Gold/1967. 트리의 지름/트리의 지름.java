74e0fa674d4861037359a8a532728b56ca0753a9
Kang JeongHyun <85155789+taboowiths@users.noreply.github.com>
2025-01-17 00:07:38 +0900

    [Gold IV] Title: 트리의 지름, Time: 156 ms, Memory: 20228 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,45 +1,45 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
-    static int N, maxValue, maxNode;
+    static int max, maxNode;
     static List<int[]>[] graph;
     static boolean[] visited;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        graph = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+        int N = Integer.parseInt(br.readLine());
+        graph = new List[N+1];
+        for (int i = 1; i <= N; i++) graph[i] = new ArrayList<>();
 
         StringTokenizer st;
         for (int i = 0; i < N-1; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int P = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
-            int W = Integer.parseInt(st.nextToken());
-            graph[P].add(new int[] {C, W});
-            graph[C].add(new int[] {P, W});
+           st = new StringTokenizer(br.readLine(), " ");
+           int from = Integer.parseInt(st.nextToken());
+           int to = Integer.parseInt(st.nextToken());
+           int weight = Integer.parseInt(st.nextToken());
+           graph[from].add(new int[] {to, weight});
+           graph[to].add(new int[] {from, weight});
         }
+
+        max = Integer.MIN_VALUE;
         visited = new boolean[N+1];
-        maxValue = -1;
         dfs(1, 0);
 
-        maxValue = -1;
-        Arrays.fill(visited, false);
+        max = Integer.MIN_VALUE;
+        visited = new boolean[N+1];
         dfs(maxNode, 0);
-        System.out.println(maxValue);
+        System.out.println(max);
     }
-
-    public static void dfs (int cur, int val) {
-        visited[cur] = true;
-        if (maxValue < val) {
-            maxValue = val;
-            maxNode = cur;
+    // 루트 1에서 가장 긴 것 -> 가장 긴 것부터 가장 긴 것
+    public static void dfs (int i, int sum) {
+        visited[i] = true;
+        if (sum > max) {
+            max = sum;
+            maxNode = i;
         }
-        for (int[] node : graph[cur]) {
-            if (visited[node[0]]) continue;
-            dfs(node[0], val + node[1]);
+        for (int[] j : graph[i]) {
+            if (visited[j[0]]) continue;
+            dfs(j[0], sum + j[1]);
         }
     }
 }
