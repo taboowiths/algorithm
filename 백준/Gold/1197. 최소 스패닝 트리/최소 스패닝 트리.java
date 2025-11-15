b908cfd7b8082837bf26c56d75097f6e45277c38
Kang JeongHyun <85155789+taboowiths@users.noreply.github.com>
2025-11-16 02:03:29 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 456 ms, Memory: 49640 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,49 +1,89 @@
+
 import java.io.*;
 import java.util.*;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 52462
+ */
 public class Main {
-    static int N, parents[], edgeList[][];
+    static class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        int weight;
+       
+        Edge (int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+        
+        public int compareTo(Edge other) {
+            return Integer.compare(this.weight, other.weight);
+        }
+    }
+
+    static int[] parents;   
+    static int[] ranks;
     public static void main(String[] args) throws Exception {
-        BufferedReader br =new BufferedReader( new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
-        edgeList = new int[E][3];
+
+        List<Edge> graph = new ArrayList<>();
+
         for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
-            edgeList[i] = new int[] {from, to, weight};
+            graph.add(new Edge(from, to, weight));
         }
-        Arrays.sort(edgeList, (o1, o2) -> Integer.compare(o1[2], o2[2])); // 간선비용의 오름차순정렬
-        makeSet();
 
-        int result = 0, cnt = 0;
-        for (int[] edge : edgeList) {
-            if (union(edge[0], edge[1])) {
-                result += edge[2];
-                if (++cnt == N - 1) break;
+        Collections.sort(graph);
+
+        parents = new int[V+1];
+        ranks = new int[V+1];
+        for (int i = 1; i <= V; i++) {
+            parents[i] = i;
+        }
+
+        int answer = 0;
+        int cnt = 0;
+        for (Edge edge : graph) {
+            if (union(edge.from, edge.to)) {
+                answer += edge.weight;
+                if (++cnt == V-1) break;
             }
         }
-        System.out.println(result);
+
+        System.out.println(answer);
+    }   
+
+    public static int find (int a) {
+        if (parents[a] == a) return a;
+        return parents[a] = find(parents[a]);
     }
 
-    static void makeSet(){
-        parents=new int[N+1];
-        for(int i=1; i<=N; i++){
-            parents[i]=i;
+    public static boolean union (int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        if (aRoot == bRoot) return false;
+
+        if (ranks[aRoot] > ranks[bRoot]) {
+            parents[bRoot] = aRoot;
+        } else if (ranks[aRoot] < ranks[bRoot]) {
+            parents[aRoot] = bRoot;
+        } else {
+            parents[bRoot] = aRoot;
+            ranks[aRoot]++;
         }
-    }
-    static int findSet(int a){
-        if(a==parents[a]) return a;
-        return parents[a]=findSet(parents[a]);
-    }
-
-    static boolean union(int a, int b){
-        int aRoot=findSet(a);
-        int bRoot=findSet(b);
-        if(aRoot==bRoot) return false;
-        parents[bRoot]=aRoot;
         return true;
     }
 }
