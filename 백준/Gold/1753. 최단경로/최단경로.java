a809023a68e00549168d43c6e26e24786cad68da
Kang JeongHyun <85155789+taboowiths@users.noreply.github.com>
2025-11-15 19:16:28 +0900

    [Gold IV] Title: 최단경로, Time: 604 ms, Memory: 116696 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,54 +1,89 @@
-import java.io.*;
-import java.util.*;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 52462
+ */
 public class Main {
-    public static void main(String[] args) throws Exception  {
+    static class Edge implements Comparable<Edge> {
+        int to; 
+        int weight;
+        Edge (int to, int weight) {
+            this.to = to;
+            this.weight = weight; 
+        }
+
+        public int compareTo(Edge other) {
+            return this.weight - other.weight;
+        }
+    }
+    
+    static List<List<Edge>> graph;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
-        int start = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
 
-        List<int[]>[] graph = new List[V+1];
-        for (int i = 0; i <= V; i++) graph[i] = new ArrayList<>();
+        graph = new ArrayList<>();
+        for (int i = 0; i <= V; i++) {
+            graph.add(new ArrayList<>());
+        }
 
-        int[] distance = new int[V+1];
-        for (int i = 1; i <= V; i++) distance[i] = Integer.MAX_VALUE;
-
-        boolean[] visited = new boolean[V+1];
-
-        for (int i = 1; i <= E; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
-            graph[from].add(new int[] {to, weight});
+            graph.get(from).add(new Edge(to, weight));
         }
 
-        PriorityQueue<int[]> pq=new PriorityQueue<>((o1,o2)->Integer.compare(o1[1],o2[1]));
-        distance[start] = 0;
-        pq.offer(new int[] {start, distance[start]});
+        int[] dist = new int[V+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(K, 0));
+        dist[K] = 0;
 
         while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            if (visited[cur[0]]) continue;
+            Edge cur = pq.poll();
 
-            visited[cur[0]] = true;
+            if (cur.weight > dist[cur.to]) continue;
 
-            for (int[] j : graph[cur[0]]) {
-                if (!visited[j[0]] && distance[j[0]] > distance[cur[0]] + j[1]) {
-                    distance[j[0]] = distance[cur[0]] + j[1];
-                    pq.offer(new int[] {j[0], distance[j[0]]});
+            for (Edge next : graph.get(cur.to)) {
+                
+                // cur.to와 연결된 모든 next 를 살펴보면서 각각의 최소 거리를 업데이트 함.
+                // dist[next] = Math.min((cur까지의 최단경로 + cur-next까지의 경로), 현재 dist[next])
+
+                int newWeight = cur.weight + next.weight;
+                if (newWeight < dist[next.to]) {
+                    dist[next.to] = newWeight;
+                    pq.add(new Edge(next.to, newWeight));
                 }
             }
         }
 
         StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i <= V; i++) {
-            if (distance[i] == Integer.MAX_VALUE) sb.append("INF");
-            else sb.append(distance[i]);
-            sb.append("\n");
+            if (dist[i] == Integer.MAX_VALUE) sb.append("INF").append("\n");
+            else sb.append(dist[i]).append("\n");
         }
-        System.out.println(sb);
+
+        System.out.println(sb.toString());
+        br.close();
     }
 }
